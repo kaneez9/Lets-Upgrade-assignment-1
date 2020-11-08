bfe2a47f42be78e660de5e6f962c153061f17a0c
kaneez9 <74129834+kaneez9@users.noreply.github.com>
2020-11-08 12:42:14 +0530

    Add files via upload

---
+++ Assignment day 1.py
@@ -0,0 +1,22 @@
+#Assignment Day1
+# Question 1
+print("1.B.RAINBOW\n")
+#Question 2
+print("LETS UPGRADE\n")
+#Question 3
+cp=int(input())
+sp=int(input())
+if(sp>cp):
+    p=sp-cp
+    print("Profit")
+if(sp<cp):
+    p=cp-sp
+    print("Loss")
+else:
+    print("Neither")
+#Question 4
+euros=int(input())
+rupees=80*euros
+print(rupees)
+      
+ 
